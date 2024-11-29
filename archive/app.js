bff202ce565dc302da27f8ed1db1990161ef4935
samir-100 <patel.samir3200@gmail.com>
2024-11-28 23:33:08 -0500

    deleted archives

--- archive/app.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// MongoDB connection URL
-const mongoURL = 'mongodb+srv://4010group:quickwhip123@quickwhip.kpxfz.mongodb.net/'; 
-
-// Connect to MongoDB
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
